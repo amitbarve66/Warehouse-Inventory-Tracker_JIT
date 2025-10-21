420e416038a2fe6d40e5159e5834f645d9e41ef9
amitbarve66 <amitbarve2022@gmail.com>
2025-10-21 20:22:34 +0530

    adde persistence layer

---
+++ src/main/java/com/amit/warehouse/persistence/InventoryPersistence.java
@@ -0,0 +1,14 @@
+package com.amit.warehouse.persistence;
+
+import com.amit.warehouse.model.Product;
+
+import java.util.Map;
+
+public class InventoryPersistence {
+    public static void loadInventory(String filename, Map<String, Product> inventory){
+
+    }
+    public static void saveInventory(String filename, Map<String, Product> inventory) {
+
+    }
+}
